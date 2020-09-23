17b27ed7f2582e999c025d05d49c7f412d5dfe7a
Paul Colta <coltapaulstefan@gmail.com>
2020-09-23 13:38:18 +0000

    Remove unused imports from IndexHelper

--- src/main/java/com/google/sps/internal/IndexHelper.java
+++ src/main/java/com/google/sps/internal/IndexHelper.java
@@ -14,34 +14,9 @@
 
 package com.google.sps.servlets;
 
-import com.google.appengine.api.datastore.DatastoreService;
-import com.google.appengine.api.datastore.DatastoreServiceFactory;
 import com.google.appengine.api.datastore.Entity;
-import com.google.appengine.api.datastore.EntityNotFoundException;
-import com.google.appengine.api.datastore.FetchOptions;
-import com.google.appengine.api.datastore.Key;
-import com.google.appengine.api.datastore.KeyFactory;
-import com.google.appengine.api.datastore.PreparedQuery;
-import com.google.appengine.api.datastore.Query.CompositeFilter;
-import com.google.appengine.api.datastore.Query.CompositeFilterOperator;
-import com.google.appengine.api.datastore.Query.Filter;
-import com.google.appengine.api.datastore.Query.FilterOperator;
-import com.google.appengine.api.datastore.Query.FilterPredicate;
-import com.google.appengine.api.datastore.Query.SortDirection;
 import com.google.appengine.api.search.*;
-import java.io.IOException;
-import java.lang.Math;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import org.apache.commons.lang3.StringUtils;
 
 public class IndexHelper {
   // Name of the index used.
@@ -55,35 +30,41 @@ public class IndexHelper {
     Index index = SearchServiceFactory.getSearchService().getIndex(indexSpec);
     return index;
   }
-  
+
   /**
-  * Adds a Recipe to the index.
-  */
-  public static void addRecipe(Entity recipeEntity, ArrayList<String> ingredients){
+   * Adds a Recipe to the index.
+   */
+  public static void addRecipe(
+    Entity recipeEntity,
+    ArrayList<String> ingredients
+  ) {
     Index index = getIndex();
-    Document recipeDocument = buildRecipeDocumentForIndexing(recipeEntity, ingredients);
+    Document recipeDocument = buildRecipeDocumentForIndexing(
+      recipeEntity,
+      ingredients
+    );
     index.put(recipeDocument);
   }
 
   /**
-  * Deletes a Recipe from the index.
-  */
-  public static void deleteRecipe(Entity recipeEntity){
+   * Deletes a Recipe from the index.
+   */
+  public static void deleteRecipe(Entity recipeEntity) {
     Index index = getIndex();
     index.delete(String.valueOf(recipeEntity.getKey().getId()));
   }
 
   /**
-  * Search and returns a list of Recipe Documents that matches the criterias from query.
-  */
+   * Search and returns a list of Recipe Documents that matches the criterias from query.
+   */
   public static Results<ScoredDocument> searchRecipes(Query query) {
-    Index index = getIndex();   
+    Index index = getIndex();
     return index.search(query);
   }
 
   /**
-  * Builds a Recipe Document to be added to the index.
-  */
+   * Builds a Recipe Document to be added to the index.
+   */
   private static Document buildRecipeDocumentForIndexing(
     Entity recipeEntity,
     ArrayList<String> ingredients
